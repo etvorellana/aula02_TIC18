075bc9f88daa379d7a7e8d05cb71024d8c76bc2f
etvorellana <evalero@uesc.br>
2023-08-22 19:19:14 -0300

    modificando qualSeuNome.cpp

--- src/qualSeuNome.cpp
+++ src/qualSeuNome.cpp
@@ -6,9 +6,16 @@ using namespace std;
 int main(void)
 {
    string nome;
+   int anoNascimento;
    cout << "Digite o seu nome: ";
    cin >> nome;
    cout << "Bom dia, " << nome << ", tenha um bom curso.";
    cout << endl;
+   cout << nome << ", voce nasceu em que ano? ";
+   cin >> anoNascimento;
+   cout << "Consderando que estamos em 2023, ou voce tem "
+        << 2023 - anoNascimento
+        << " anos, ou esta proximo de fazer."
+        << endl;
    return 0;
 }
