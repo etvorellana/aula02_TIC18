c2bf71833f4d6428086202243d5e56d80ed6636e
etvorellana <evalero@uesc.br>
2023-08-22 19:09:22 -0300

    Adicionanddo novo exemplo na pasta src

---
+++ src/qualSeuNome.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+   string nome;
+   cout << "Digite o seu nome: ";
+   cin >> nome;
+   cout << "Bom dia, " << nome << ", tenha um bom curso.";
+   cout << endl;
+   return 0;
+}
